c0b77c2d0cffb6c85d20b6cb76a86b248b9501e5
Marty Sama <martysama0134@users.noreply.github.com>
2023-12-05 16:40:58 +0100

    Update optionparser.h with Required ArgStatus

--- optionparser.h
+++ optionparser.h
@@ -933,8 +933,42 @@ struct Arg
   {
     if (option.arg && option.name[option.namelen] != 0)
       return ARG_OK;
-    else
-      return ARG_IGNORE;
+    return ARG_IGNORE;
+  }
+
+  static void printError(const char* msg1, const Option& opt, const char* msg2)
+  {
+      fprintf(stderr, "%s", msg1);
+      fwrite(opt.name, opt.namelen, 1, stderr);
+      fprintf(stderr, "%s", msg2);
+  }
+  static ArgStatus Unknown(const Option& option, bool msg)
+  {
+      if (msg) printError("Unknown option '", option, "'\n");
+      return ARG_ILLEGAL;
+  }
+  static ArgStatus Required(const Option& option, bool msg)
+  {
+      if (option.arg != 0)
+          return ARG_OK;
+      if (msg) printError("Option '", option, "' requires an argument\n");
+      return ARG_ILLEGAL;
+  }
+  static ArgStatus NonEmpty(const Option& option, bool msg)
+  {
+      if (option.arg != 0 && option.arg[0] != 0)
+          return ARG_OK;
+      if (msg) printError("Option '", option, "' requires a non-empty argument\n");
+      return ARG_ILLEGAL;
+  }
+  static ArgStatus Numeric(const Option& option, bool msg)
+  {
+      char* endptr = 0;
+      if (option.arg != 0 && strtol(option.arg, &endptr, 10)) {};
+      if (endptr != option.arg && *endptr == 0)
+          return ARG_OK;
+      if (msg) printError("Option '", option, "' requires a numeric argument\n");
+      return ARG_ILLEGAL;
   }
 };
 
